330743393f0913b3b7662b7ca30b9b7dad9846a2
szzi <kamuuim@naver.com>
2019-05-31 00:20:54 +0900

    table data 가져오기

---
+++ acc.php
@@ -0,0 +1,34 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);  //에러 발생시 표시하기 위한 부분
+
+//-- DB 연결
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '111111',
+    'cleaning');
+
+include ("configure.php");
+
+echo "<h1>ACC</h1>";
+$sql = "SELECT * FROM acc";
+$result = mysqli_query($conn, $sql);
+
+echo <<< EOP
+<table width="60%" border=1 cellspacing=0 cellpadding=5>
+<tr>
+<td>name</td><td>price</td>
+</tr>
+EOP;
+
+while($row = mysqli_fetch_array($result)) {
+    echo '<tr>';
+    echo '<td>'.$row['name'].'</td>';
+    echo '<td>'.$row['price'].'원</td>';
+    echo '</tr>';
+}
+echo '</table>'
+?>
+
+
--- top.php
+++ top.php
@@ -1,40 +1,34 @@
-<?
+<?php
 error_reporting(E_ALL);
 ini_set("display_errors", 1);  //에러 발생시 표시하기 위한 부분
 
 //-- DB 연결
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '111111',
+    'cleaning');
+
 include ("configure.php");
-include ("connectlist.php");
 
-$connect=connect_db($host, $dbid, $dbpw, $dbname); 
-mysql_select_db($mysql_db, $connect); // DB 선택
+echo "<h1>TOP</h1>";
+$sql = "SELECT * FROM top";
+$result = mysqli_query($conn, $sql);
 
-$sql = "select * from top";
-$rs = mysql_query($sql);
-?>
-
-<style>
-.title { text-align:center; background:#EBEBEB; }
-.num { text-align:center; }
-</style>
-
+echo <<< EOP
 <table width="60%" border=1 cellspacing=0 cellpadding=5>
-<tr class="title">
-<td>type</td><td>name</td><td>price</td>
+<tr>
+<td>name</td><td>price</td>
 </tr>
-<? 
-while($rd = mysql_fetch_array($rs)){
-    echo "<tr>";
-    echo "<td>".$rd[type]."</td>";
-    // <td>
-    //     .$rd[name]
-    // </td>
-    // <td>
-    //     .$rd[price]원
-    // </td>
-</tr>
-";
+EOP;
+
+while($row = mysqli_fetch_array($result)) {
+    echo '<tr>';
+    echo '<td>'.$row['name'].'</td>';
+    echo '<td>'.$row['price'].'원</td>';
+    echo '</tr>';
 }
-mysql_close();
+echo '</table>'
 ?>
-</table> 
+
+
